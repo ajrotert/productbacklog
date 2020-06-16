22794f7d4c5932fec4a9524e582142dabd887873
andrewrotert <Andrew.Rotert@trojans.dsu.edu>
2020-06-15 21:11:07 -0500

    updates

--- Scripts/react-pbm.js
+++ Scripts/react-pbm.js
@@ -2,16 +2,7 @@
 var db = firebase.firestore();
 const uid = sessionStorage.getItem('uid');
 
-if (uid != null) {
-    db.collection(uid).get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                var local = doc.data();
-                if(local!=null)
-                    console.log(local.title + " " + local.description);
-            });
-    });
-}
+    
 
 class NotAuthError extends React.Component {
     render() {
@@ -25,15 +16,51 @@ class NotAuthError extends React.Component {
 }
 
 class PBI extends React.Component {
+
     render() {
         return (
             <div>
+                <ul>{this.props.id}
+                    <li>{this.props.title}</li>
+                    <li>{this.props.description}</li>
+                </ul>
+            </div>
+        );
+    }
+}
+class PB extends React.Component {
+    renderPBI(id, title, description) {
+        return (
+            <PBI id={id} title={title} description={description} />
+            );
+    }
 
+    render() {
+        //NOT WORKING HERE. JUST SAVING
+
+        const PBIContainer = temp.map((index, array) => {
+            db.collection(uid).get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        var local = doc.data();
+                        if (local != null) {
+                            <div>{this.renderPBI(doc.id, local.title, local.description)}</div>
+                        }
+                    })
+                });
+        });
+
+        return (
+            <div>
+                {PBIContainer}
             </div>
         );
     }
 }
 
 const domContainer = document.querySelector('#root');
-if(uid==null)
-    ReactDOM.render(<NotAuthError/>, domContainer);
+if (uid == null)
+    ReactDOM.render(<NotAuthError />, domContainer);
+else {
+    ReactDOM.render(<PB />, domContainer);
+}
