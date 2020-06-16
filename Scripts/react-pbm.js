cd780f8f89f349b078eb05c199939ac937830777
andrewrotert <Andrew.Rotert@trojans.dsu.edu>
2020-06-16 15:55:42 -0500

    Css-Grid, Firebase content loads, react renders

--- Scripts/react-pbm.js
+++ Scripts/react-pbm.js
@@ -16,22 +16,28 @@ class NotAuthError extends React.Component {
 }
 
 class PBI extends React.Component {
+    state() {
+
+    }
 
     render() {
         return (
-            <div>
-                <ul>{this.props.id}
-                    <li>{this.props.title}</li>
-                    <li>{this.props.description}</li>
-                </ul>
+            <div className="PBI box_shadow">
+                <h1>{this.props.title}</h1>
+                <hr />
+                <p>Description: {this.props.description}</p>
+                <input type="checkbox" id="done" name="done" value={this.props.completed}/>
+                    <label htmlFor="done"> Item Completed</label><br />
+                <p>ID: {this.props.id}</p>
+                
             </div>
         );
     }
 }
 class PB extends React.Component {
-    renderPBI(id, title, description) {
+    renderPBI(id, title, description, completed) {
         return (
-            <PBI id={id} title={title} description={description} />
+            <PBI id={id} title={title} description={description} completed={completed} />
             );
     }
 
@@ -39,12 +45,18 @@ class PB extends React.Component {
 
         const PBIContainer = this.props.data.docs.map((object, index) => {
             return (
-                <div key={index}>{this.renderPBI(object.id, object.data().title, object.data().description)}</div>
+                <div className="grid_item_one grid_border_right" key={index}>{this.renderPBI(object.id, object.data().title, object.data().description, object.data().completed)}</div>
                 );
         });
 
         return (
-            <div>
+            <div className="grid-container">
+                <h1 className="grid_item_one grid_border_bottom">Backlog</h1>
+                <h1 className="grid_item_two grid_border_bottom">Completed</h1>
+                <div className="grid_item_one grid_border_right">
+                   <a className="button" onClick="">New Item</a>
+                </div>
+
                 {PBIContainer}
             </div>
         );
