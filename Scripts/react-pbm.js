47e4c588eccf598d151158d44223673cf316c265
andrewrotert <Andrew.Rotert@trojans.dsu.edu>
2020-06-16 22:20:44 -0500

    Realtime database updates. Modal popups.

--- Scripts/react-pbm.js
+++ Scripts/react-pbm.js
@@ -15,19 +15,58 @@ class NotAuthError extends React.Component {
     }
 }
 
-class PBI extends React.Component {
-    state() {
-
+class ModalView extends React.Component {
+    handler() {
+        var modal = document.getElementById("InputModal")
+        modal.style.display = "none";
     }
 
+    addToDatabase() {
+        var title = document.getElementById('title').value;
+        var description = document.getElementById('description').value;
+        console.log(title + " " + description + " " + uid);
+        if (title != "" && description != "" && uid != null) {
+
+            db.collection(uid).doc().set({
+                title: title,
+                description: description,
+                completed: false
+            });
+            document.getElementById("InputModal").style.display = "none";
+        }
+    }
+
+    render() {
+        return (
+            <div id="InputModal" className="modal">
+                <div className="modal-content">
+                    <span className="close" onClick={this.handler}>&times;</span>
+                    <h1>Please enter the following information: </h1>
+                    <label htmlFor="title">Title for Product Backlog Item: </label><br/>
+                    <input id="title" type="textbox" name="title" placeholder="Enter title"/>
+                    <br />
+                    <br />
+                    <label htmlFor="description">Product Backlog Item Description: </label><br/>
+                    <textarea id="description" name="description" placeholder="Enter description"/>
+                    <br />
+                    <br />
+                    <button onClick={this.addToDatabase}>Submit</button>
+                    <br/>
+                </div>
+            </div>
+            );
+    }
+}
+
+class PBI extends React.Component {
     render() {
         return (
             <div className="PBI box_shadow">
                 <h1>{this.props.title}</h1>
                 <hr />
                 <p>Description: {this.props.description}</p>
-                <input type="checkbox" id="done" name="done" value={this.props.completed}/>
-                    <label htmlFor="done"> Item Completed</label><br />
+                <input type="checkbox" id={"done" + this.props.id} name={"done" + this.props.id} value={this.props.completed}/>
+                <label htmlFor={"done" + this.props.id }> Item Completed</label><br />
                 <p>ID: {this.props.id}</p>
                 
             </div>
@@ -35,10 +74,22 @@ class PBI extends React.Component {
     }
 }
 class PB extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            obj: null
+        }
+    }
     renderPBI(id, title, description, completed) {
         return (
             <PBI id={id} title={title} description={description} completed={completed} />
             );
+    }
+
+    handler(){
+        var modal = document.getElementById("InputModal");
+        // When the user clicks the button, open the modal 
+        modal.style.display = "block";
     }
 
     render() {
@@ -54,7 +105,7 @@ class PB extends React.Component {
                 <h1 className="grid_item_one grid_border_bottom">Backlog</h1>
                 <h1 className="grid_item_two grid_border_bottom">Completed</h1>
                 <div className="grid_item_one grid_border_right">
-                   <a className="button" onClick="">New Item</a>
+                    <a className="button" onClick={this.handler}>New Item</a>
                 </div>
 
                 {PBIContainer}
@@ -68,8 +119,17 @@ if (uid == null)
     ReactDOM.render(<NotAuthError />, domContainer);
 else {
 
-    db.collection(uid).get()
-        .then((snapshot) => {
+    db.collection(uid)
+        .onSnapshot((snapshot) => {
             ReactDOM.render(<PB data={snapshot} />, domContainer);
         })
+    ReactDOM.render(<ModalView />, document.querySelector('#rootModal'));
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    var modal = document.getElementById("InputModal")
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
